b8cd8260d2ff97bb316a6ca8269125cb60492839
Davide Costa <davide@sardu.pro>
2026-09-07 21:04:01 +0200

    fix-drawtext-toolbox-instance

--- matrix.ts
+++ matrix.ts
@@ -102,7 +102,7 @@ namespace sarduMatrix {
 
         /** Draws static text at explicit coordinates without showing it. */
         // Keep block placeholders in the same order as the TypeScript parameters.
-        //% blockId=sardu_matrix_draw_text block="$this draw static text $text at x $x y $y|color $color=neopixel_colors font $font size $size brightness $brightness orientation $orientation"
+        //% blockId=sardu_matrix_draw_text block="$this=variables_get(matrix) draw static text $text at x $x y $y|color $color=neopixel_colors font $font size $size brightness $brightness orientation $orientation"
         //% group="Static text" weight=80 help=github:pxt-sardu-matrix/docs/api
         //% this.shadow=variables_get this.defl=matrix text.defl="Hello" x.defl=0 y.defl=0 font.defl=MatrixFont.Sardu size.defl=MatrixFontSize.X1 color.shadow=neopixel_colors brightness.min=0 brightness.max=255 brightness.defl=128 orientation.defl=MatrixTextOrientation.Normal
         drawText(
